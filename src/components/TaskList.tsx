323574df2d160d2cfa8ae8d950bc5b0761096390
Rodrigo Godoy MacBook <rodrigocgodoy@hotmail.com>
2021-03-18 23:49:09 -0300

    Modified rules on code

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,38 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) {
+      return;
+    }
+    let id: number;
+    if (tasks.length > 0) {
+      id = tasks[tasks.length - 1]?.id + 1;
+    } else {
+      id = 1;
+    }
+    setTasks([...tasks, {
+      id,
+      title: newTaskTitle,
+      isComplete: false,
+    }]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const tasksData = tasks?.map((task) => {
+      if (task?.id === id) {
+        task.isComplete = !task.isComplete;
+      }
+      return task;
+    });
+    setTasks([...tasksData]);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const tasksData = tasks.filter((task) => task?.id !== id);
+    setTasks([...tasksData]);
   }
 
   return (
